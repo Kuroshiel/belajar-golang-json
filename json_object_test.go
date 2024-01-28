bc2b2f99b107d65aaf9646fc98949658b4975bb2
Kuroshiel <alifianfikry@gmail.com>
2024-01-28 13:05:23 +0700

    Learn JSON Object Golang Web

---
+++ json_object_test.go
@@ -0,0 +1,30 @@
+package belajargolangjson
+
+import (
+	"encoding/json"
+	"fmt"
+	"testing"
+)
+
+// JSON Object Golang Web
+
+type Customer struct {
+	FirstName  string
+	MiddleName string
+	LastName   string
+	Age        int
+	Married    bool
+}
+
+func TestJSONObject(t *testing.T) {
+	customer := Customer{
+		FirstName:  "Eko",
+		MiddleName: "Kurniawan",
+		LastName:   "Khannedy",
+		Age:        30,
+		Married:    true,
+	}
+
+	bytes, _ := json.Marshal(customer)
+	fmt.Println(string(bytes))
+}
